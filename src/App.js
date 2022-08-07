fde354c62d9bb9f666954eaa64dd26c807d1fd37
Fred Pope <fred@fredpope.com>
2022-08-07 17:35:50 -0600

    Testing Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/sslogo.png';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
