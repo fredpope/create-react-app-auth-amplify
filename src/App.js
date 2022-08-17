6cfea6bf9559649f4c32980cd24e01592c16de79
Fred Pope <fred@fredpope.com>
2022-08-16 23:09:49 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -1,185 +1,30 @@
-import React, { Component, useEffect, useState } from 'react';
-import logo from './images/sslogo.png';
-import { ethers } from "ethers";
-import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-import { Button, Card } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Amplify } from 'aws-amplify';
 
-Amplify.configure(aws_exports);
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-/**
- * class App extends Component {
-  
-  render() {
-    return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
-    );
-  }
-}
-*/
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-function App() {
-
-  const isMobileDevice = () => {
-    return 'ontouchstart' in window || 'onmsgesturechange' in window;
-  };
-
-  const [data, setdata] = useState({
-    address: "",
-    Balance: null,
-  });
-
-  const btnhandler = () => {
-  
-    // Asking if metamask is already present or not
-    if (window.ethereum) {
-  
-      // res[0] for fetching a first wallet
-      window.ethereum
-        .request({ method: "eth_requestAccounts" })
-        .then((res) => accountChangeHandler(res[0]));
-    } else {
-      alert("install metamask extension!!!");
-    }
-  };
-
-  const getbalance = (address) => {
-  
-    // Requesting balance method
-    window.ethereum
-      .request({ 
-        method: "eth_getBalance", 
-        params: [address, "latest"] 
-      })
-      .then((balance) => {
-        // Setting balance
-        setdata({
-          Balance: ethers.utils.formatEther(balance),
-        });
-      });
-  };
-
-  const accountChangeHandler = (account) => {
-    // Setting an address data
-    setdata({
-      address: account,
-    });
-  
-    // Setting a balance
-    getbalance(account);
-  };
-
-  return (
-    <div>
-      <AmplifySignOut />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Card className="text-center">
-        <Card.Header>
-          <strong>Address: </strong>
-          {data.address}
-        </Card.Header>
-        <Card.Body>
-          <Card.Text>
-            <strong>Balance: </strong>
-            {data.Balance}
-          </Card.Text>
-          <Button onClick={btnhandler} variant="primary">
-            Connect to wallet
-          </Button>
-        </Card.Body>
-      </Card>
-    </div>
-  );
-}
-
-async function checkIfWalletIsConnected(onConnected) {
-  if (window.ethereum) {
-    const accounts = await window.ethereum.request({
-      method: "eth_accounts",
-    });
-
-    if (accounts.length > 0) {
-      const account = accounts[0];
-      onConnected(account);
-      return;
-    }
-
-    if (isMobileDevice()) {
-      await connect(onConnected);
-    }
-  }
-};
-
-function MetaMaskAuth({ onAddressChanged }) {
-  const [userAddress, setUserAddress] = useState("");
-
-  useEffect(() => {
-    checkIfWalletIsConnected(setUserAddress);
-  }, []);
-
-  useEffect(() => {
-    onAddressChanged(userAddress);
-  }, [userAddress]);
-
-  return userAddress ? (
-    <div>
-      Connected with <Address userAddress={userAddress} />
-    </div>
-  ) : (
-     <Connect setUserAddress={setUserAddress}/>
-  );
-}
-
-async function connect(onConnected) {
-  if (!window.ethereum) {
-    alert("Get MetaMask!");
-    return;
-  }
-
-  const accounts = await window.ethereum.request({
-    method: "eth_requestAccounts",
-  });
-
-  onConnected(accounts[0]);
-}
-
-function Connect({ setUserAddress }) {
-  if (isMobileDevice()) {
-    const dappUrl = "www.sureshake.com"; // TODO enter your dapp URL. For example: https://uniswap.exchange. (don't enter the "https://")
-    const metamaskAppDeepLink = "https://metamask.app.link/dapp/" + dappUrl;
-    return (
-      <a href={metamaskAppDeepLink}>
-         <button variant="primary">
-           Connect to MetaMask
-         </button>
-      </a>
-    );
+function App({ isPassedToWithAuthenticator, signOut, user }) {
+  if (!isPassedToWithAuthenticator) {
+    throw new Error(`isPassedToWithAuthenticator was not provided`);
   }
 
   return (
-    <button variant="primary" onClick={() => connect(setUserAddress)}>
-      Connect to MetaMask
-    </button>
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
-function Address({ userAddress }) {
-  return (
-    <span>{userAddress.substring(0, 5)}…{userAddress.substring(userAddress.length - 4)}</span>
-  );
-}
-
-function isMobileDevice() {
-  return 'ontouchstart' in window || 'onmsgesturechange' in window;
-}
 export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
+} 
